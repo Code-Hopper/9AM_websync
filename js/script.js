f6f53a1eb45901f31693dc6007f71159ae9a9837
code-hopper <ameykhondekar01@gmail.com>
2024-08-27 11:08:01 +0530

    js day2

---
+++ js/script.js
@@ -0,0 +1,81 @@
+// variables & data types 
+
+// variable: a empty box which may have some name where data can be stored and called.
+
+// ; for termination of a line or statement
+
+// syntax for variable
+
+// scope variable_name assignment-oprator data/value(optional);
+
+// public (var), protected (let, const)
+// rules for naming a variable
+// the has be unique, no special char(!@#$%^&*),no space in name (first name) (first_name), name can be alpha-numeric (name123) but cannot start with a number name_1(right), 1name(wrong)   
+// assignment oprator , = , += , -= , /= , %= , *=
+
+// write a opration adding 2 numbers and store the total in number1
+
+// value = number1 + number2
+
+// number1 += number2
+// number1 -= number2
+// number1 /= number2
+// number1 %= number2
+// number1 *= number2
+
+// to write value/data we have some data types
+// numbers (integers(1,2,3,4,5,...)and floats(1.1,1.2,1.3,1.4,......)), 
+// string (group for charchters) we write string in -> " some string "
+// boolean (true/false)
+// NaN, Null, Undefined
+
+// let box_1 = 1234
+// let box_2 = "this is a string"
+// let box_3 = true
+// let box_4 = false
+// let box_5 = 10.5 
+
+// expersion and statement
+
+// oprators in js
+
+// 1. airthamatic oprators + , - , / , * , %
+// increement and decreement oprators ++ , --
+// 2. assignment oprators = , +=,-=,/=,%=,*=
+// 3. conditional(comparision oprators) oprators < , > , <= , >=, == , === , != 
+// 4. logical oprators && (and), ||(or)
+// extra oprators, .
+
+// functions, small block of code that can be called again and again as needed !
+
+// built in function , user defined function
+
+// built in function input & output function
+// alert(), console.log() - output function
+
+// console.log("this is some message")
+// console.log("this is some message")
+// console.log("this is some message")
+// console.log("this is some message")
+// console.log("this is some message")
+// console.log("this is some message")
+// console.log("this is some message")
+// console.log("this is some message")
+// console.log("this is some message")
+
+// alert("this is also a message")
+
+// input fuinction in js
+
+// let name = prompt("enter some data !")
+
+// console.log("data from prompt is : ")
+// console.log(name)
+
+// function_name("something") , 
+
+// typeof() this function returns the datatype of passed args 
+
+let data = Number("1a")
+console.log(typeof(data))
+console.log(data)
