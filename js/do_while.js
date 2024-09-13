8c07514c62bd91725087b5ee7db93d088f6ae6d5
code-hopper <ameykhondekar01@gmail.com>
2024-09-13 11:17:57 +0530

    do while and for loop

---
+++ js/do_while.js
@@ -0,0 +1,26 @@
+// // let number = 10
+
+// // while(number < 10){
+// //     console.log(number)
+// //     number++
+// // }
+
+// // // do{
+// // //     console.log(number)
+// // //     number++
+// // // }while(number < 10)
+
+
+// let greet = () => {
+//     console.log("hello ! im greet function")
+// }
+
+// let choice = "no"
+
+// do {
+//     greet()
+//     choice = prompt("enter yes/no")
+// } while (choice === "yes")
+
+// console.log("program completed ")    
+
